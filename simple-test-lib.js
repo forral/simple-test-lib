44b10ab750c932629251ffb509764e1c0b779106
forral <forra.luis@me.com>
2017-10-16 19:39:08 -0500

    Added the table with the resume. Not the ideal styles, change in the future

--- simple-test-lib.js
+++ simple-test-lib.js
@@ -3,13 +3,16 @@ var myTestFramework = {
 		var numbOfFailures = 0;
 		var testPassed;
 		for (x in tests) {
+			this.info['Number of tests']++;
 			testPassed = tests[x];
 			testDescription = x;
 			try {
 				testPassed.apply(myTestFramework);
+				this.info['Number of successess']++;
 				console.log('%c' + testDescription, 'color: green;');
 			} catch(e) {
 				numbOfFailures++;
+				this.info['Number of failures']++;
 				console.groupCollapsed('%c' + testDescription, 'color: red;');
 				console.error(e.stack);
 				console.groupEnd();
@@ -17,6 +20,8 @@ var myTestFramework = {
 		}
 		setTimeout(function() {
 			numbOfFailures === 0 ? document.bgColor = 'green' : document.bgColor = 'red';
+			console.log("%cTests Resume Information:", "color: #BADA55; font-style: italic; font-size: 20px;");
+			console.table(myTestFramework.info);
 		}, 0);
 	},
 	fail: function() {
@@ -26,6 +31,11 @@ var myTestFramework = {
 		if (a !== b) {
 			throw new Error('Equality Test Has Failed: ' + a + ' !== ' + b);
 		}
+	},
+	info: {
+		'Number of failures': 0,
+		'Number of successess': 0,
+		'Number of tests': 0
 	}
 };
 var tests = myTestFramework.run.bind(myTestFramework);
